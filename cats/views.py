bd246c73c2dadfd28f1b3a3fa7a3798f9ff29e1c
Ilya Seleznev <blizardsis@yandex.ru>
2023-01-27 17:17:43 +0300

    1

--- cats/views.py
+++ cats/views.py
@@ -2,7 +2,6 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-
 from .models import Cat
 from .serializers import CatSerializer
 
@@ -21,5 +20,5 @@ class APICat(APIView):
         serializer = CatSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED) 
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
